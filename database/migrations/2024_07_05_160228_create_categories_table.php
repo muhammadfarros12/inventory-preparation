dad65580b2f0e22afaa8840745912b6d3b4756cf
ammaar12 <muhammadfarroos12@gmail.com>
2024-07-05 23:25:39 +0700

    set migration & admin seeder

--- database/migrations/2024_07_05_160228_create_categories_table.php
+++ database/migrations/2024_07_05_160228_create_categories_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->enum('level',['admin','pic'])->default('pic');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('category_name')->unique();
             $table->timestamps();
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
